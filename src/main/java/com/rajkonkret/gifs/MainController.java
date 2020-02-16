1a774e57b8e75da46c5a9abca1d9afd56a94f28d
rajkonkret <rajkonkret660@gmail.com>
2020-02-16 11:20:03 +0100

    add MainController to web access

---
+++ src/main/java/com/rajkonkret/gifs/MainController.java
@@ -0,0 +1,16 @@
+package com.rajkonkret.gifs;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    @ResponseBody
+    public String response(){
+        return "OK";
+    }
+
+
+
+}
