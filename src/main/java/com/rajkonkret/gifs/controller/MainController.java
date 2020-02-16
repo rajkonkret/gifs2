599a0e8c36e95e4e74ce37c649aa10ea20f8e619
rajkonkret <rajkonkret660@gmail.com>
2020-02-16 13:01:23 +0100

    Add home.html, Favorites.html and metod to gwt ALLGifs and GetFvouritesGifs

--- src/main/java/com/rajkonkret/gifs/controller/MainController.java
+++ src/main/java/com/rajkonkret/gifs/controller/MainController.java
@@ -1,29 +1,47 @@
 package com.rajkonkret.gifs.controller;
+
+import com.rajkonkret.gifs.model.Gif;
 import com.rajkonkret.gifs.repository.GifRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
     @Autowired
     GifRepository gifRepository;
-    @GetMapping("/")
-    @ResponseBody
-    public String response(){
 
-        return "OK";
+    @GetMapping("/")
+    // @ResponseBody -  nie potrzebujemy bo już zwracamy stronę
+    public String response(ModelMap modelMap) {
+        //wyciągnięcie gifów
+        List<Gif> gifList = gifRepository.getAllGifs();
+        //przekazanie do warstwy widoku
+        modelMap.put("gifs", gifList);
+        //Zwrócenie
+        return "home";
     }
 
     @GetMapping("/showgifsinbrowser")
     @ResponseBody
     public String showGifs() {
         return gifRepository.getGifsNames();
-
     }
 
+    @GetMapping("/favorites")
+    public String favoritesGifs(ModelMap modelMap) {
+        // wyciągnięcie wartości
+        List<Gif> favouritesGifs = gifRepository.getAllFavoritesGifs();
+        //przekazanie do view
+        modelMap.put("gifs", favouritesGifs);
+        // zwrócenie widoku
+        return "favorites";
+    }
 
 
 }
--- src/main/java/com/rajkonkret/gifs/repository/GifRepository.java
+++ src/main/java/com/rajkonkret/gifs/repository/GifRepository.java
@@ -11,12 +11,12 @@ import java.util.stream.Collectors;
 @Repository
 public class GifRepository {
     private static List<Gif> ALL_GIFS = Arrays.asList(
-            new Gif("android-explotion","mols",true, 1),
-            new Gif("ben and mike","mika",true, 2),
+            new Gif("android-explosion","mols",true, 1),
+            new Gif("ben-and-mike","mika",true, 2),
             new Gif("book-dominos","mem",true, 0),
             new Gif("compiler-bot","bot",true, 1),
             new Gif("cowboy-coder","code",true, 1),
-            new Gif("infinite-andrew","andrew",true, 2)
+            new Gif("infinite-andrew","andrew",false, 2)
     );
     public List<Gif> getAllGifs(){
         return ALL_GIFS;
@@ -25,4 +25,9 @@ public class GifRepository {
     public String getGifsNames(){
         return ALL_GIFS.stream().map(s -> s.getName()).collect(Collectors.joining(" , "));
     }
+
+    public List<Gif> getAllFavoritesGifs(){
+        return ALL_GIFS.stream()
+                //.filter( s -> s.getFavorite()==true).collect(Collectors.toList());}
+                .filter( s -> s.getFavorite()).collect(Collectors.toList());}
 }
